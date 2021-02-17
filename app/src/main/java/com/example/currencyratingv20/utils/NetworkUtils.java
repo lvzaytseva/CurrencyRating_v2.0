c7d3c14b9a407288bbc3e6d6eb69e9fbf1fd484c
lvzaytseva <nevadka007@yandex.ru>
2021-02-18 00:07:44 +0300

    Изменен формат данных + удален неиспользуемый метод

--- app/src/main/java/com/example/currencyratingv20/utils/NetworkUtils.java
+++ app/src/main/java/com/example/currencyratingv20/utils/NetworkUtils.java
@@ -2,7 +2,6 @@ package com.example.currencyratingv20.utils;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 import org.json.JSONException;
@@ -15,7 +14,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,27 +24,27 @@ public class NetworkUtils {
     private static final String API_KEY = "3544413c-6d6c-4e43-a325-bf19f5a000c5";
     private static final String BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
 
-
     private static final String PARAMS_START = "start";
     private static final String PARAMS_LIMIT = "limit";
     private static final String PARAMS_CONVERT = "convert";
 
     private static final String CONVERT_VALUE = "USD";
-    private static final String START_VALUE = "1"; //по умолчанию начинаем с первого
-    private static String LIMIT_VALUE = "5"; //в последствии брать значнеие из настроек
+    private static final int START_VALUE = 1;
+    private static int limitValue = 5;
 
-    public static void setLimitValue(int LIMIT_VALUE) {
-        NetworkUtils.LIMIT_VALUE = String.valueOf(LIMIT_VALUE);
+    public static void setLimitValue(int limitValue) {
+        NetworkUtils.limitValue = limitValue;
     }
-    public static int getLimitValue(){
-        return Integer.parseInt(LIMIT_VALUE);
+
+    public static int getLimitValue() {
+        return limitValue;
     }
 
     public static URL buildURL() {
         URL result = null;
         Uri uri = Uri.parse(BASE_URL).buildUpon()
-                .appendQueryParameter(PARAMS_START, START_VALUE)
-                .appendQueryParameter(PARAMS_LIMIT, LIMIT_VALUE)
+                .appendQueryParameter(PARAMS_START, String.valueOf(START_VALUE))
+                .appendQueryParameter(PARAMS_LIMIT, String.valueOf(limitValue))
                 .appendQueryParameter(PARAMS_CONVERT,CONVERT_VALUE)
                 .build();
         try {
@@ -57,60 +55,12 @@ public class NetworkUtils {
         return result;
     }
 
-    public static JSONObject getJSONFromNetwork() {
-        JSONObject result = null;
-        URL url = buildURL();
-        try {
-          result = new JSONLoadTask().execute(url).get();
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    private static class JSONLoadTask extends AsyncTask<URL, Void, JSONObject> {
-        @Override
-        protected JSONObject doInBackground(URL... urls) {
-            JSONObject result = null;
-            if (urls == null || urls.length == 0) {
-                return null;
-            }
-            HttpURLConnection connection = null;
-            try {
-                connection = (HttpURLConnection) urls[0].openConnection();
-                connection.setRequestProperty("X-CMC_PRO_API_KEY", API_KEY);
-                InputStream inputStream = connection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader (inputStream);
-                BufferedReader reader = new BufferedReader(inputStreamReader);
-                StringBuilder builder = new StringBuilder();
-                String line = reader.readLine();
-                while (line != null) {
-                    builder.append(line);
-                    line = reader.readLine();
-                }
-                result = new JSONObject(builder.toString());
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null)
-                    connection.disconnect();
-            }
-            return result;
-
-        }
-    }
-
     public static class JSONLoader extends AsyncTaskLoader<JSONObject> {
-
-        private Bundle bundle;
+        private final Bundle bundle;
         private OnStartLoadingListener onStartLoadingListener;
 
         public interface OnStartLoadingListener {
             void onStartLoading();
-        }
-
-        public void setOnStartLoadingListener(OnStartLoadingListener onStartLoadingListener) {
-            this.onStartLoadingListener = onStartLoadingListener;
         }
 
         public JSONLoader(@NonNull Context context, Bundle bundle) {
@@ -148,16 +98,17 @@ public class NetworkUtils {
             try {
                 connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestProperty("X-CMC_PRO_API_KEY", API_KEY);
-                InputStream inputStream = connection.getInputStream();
+                try (InputStream inputStream = connection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader reader = new BufferedReader(inputStreamReader);
-                StringBuilder builder = new StringBuilder();
-                String line = reader.readLine();
-                while (line != null) {
-                    builder.append(line);
-                    line = reader.readLine();
+                BufferedReader reader = new BufferedReader(inputStreamReader)) {
+                    StringBuilder builder = new StringBuilder();
+                    String line = reader.readLine();
+                    while (line != null) {
+                        builder.append(line);
+                        line = reader.readLine();
+                    }
+                    result = new JSONObject(builder.toString());
                 }
-                result = new JSONObject(builder.toString());
             } catch (IOException | JSONException e) {
                 e.printStackTrace();
             } finally {
